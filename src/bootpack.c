0b15572d0e86166450d7bf10b2138923b5cf3ecb
sksat <sksat.tec@gmail.com>
2018-03-07 19:55:14 +0900

    4-3 : ポインタに挑戦

--- src/bootpack.c
+++ src/bootpack.c
@@ -3,9 +3,13 @@ void write_mem8(int addr, int data);
 
 void HariMain(void){
 	int i; // 変数宣言．iは32bitの整数型
+	char* p; // 番地変数(ポインタ)．char*なのでBYTE [...]用の番地
 
 	for(i=0xa0000; i<= 0xaffff; i++){
-		write_mem8(i, i & 0x0f); // MOV BYTE [i], i&0x0f
+		p = (char*) i;		// 番地変数pに番地を代入
+		*p = i & 0x0f;		// pの番地のメモリに色番号を代入
+
+		// これでwrite_mem8(i, i & 0x0f); の代わりになる
 	}
 
 	for(;;){
--- src/naskfunc.nas
+++ src/naskfunc.nas
@@ -15,9 +15,3 @@
 _io_hlt:	; void io_hlt(void);
 	HLT
 	RET
-
-_write_mem8:	; void write_mem8(int addr, int data);
-	MOV	ECX, [ESP+4]	; [ESP+4]にアドレスが入っているのでそれをECXに読み込む
-	MOV	AL, [ESP+8]	; [ESP+8]にデータが入っているのでそれをALに読み込む
-	MOV	[ECX], AL
-	RET
