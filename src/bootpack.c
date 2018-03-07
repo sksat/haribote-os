5228f8372e346dbc2804321f7e519ddb632f6309
sksat <sksat.tec@gmail.com>
2018-03-07 14:46:43 +0900

    4-1 : C言語からメモリに書き込みたい

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,7 +1,14 @@
 void io_hlt(void);	// 関数のプロトタイプ宣言(外部ファイルの関数)
+void write_mem8(int addr, int data);
 
 void HariMain(void){
-fin:
-	io_hlt();	// naskfunc.nasのio_hltを実行
-	goto fin;
+	int i; // 変数宣言．iは32bitの整数型
+
+	for(i=0xa0000; i<= 0xaffff; i++){
+		write_mem8(i, 15); // MOV BYTE [i],15
+	}
+
+	for(;;){
+		io_hlt();
+	}
 }
