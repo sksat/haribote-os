f4e9b58e351a5c31c2a5ea741cd2af100d61eed9
sksat <sksat.tec@gmail.com>
2018-03-10 18:22:41 +0900

    4-4 : ポインタの応用(1)

--- src/bootpack.c
+++ src/bootpack.c
@@ -5,11 +5,10 @@ void HariMain(void){
 	int i; // 変数宣言．iは32bitの整数型
 	char* p; // 番地変数(ポインタ)．char*なのでBYTE [...]用の番地
 
-	for(i=0xa0000; i<= 0xaffff; i++){
-		p = (char*) i;		// 番地変数pに番地を代入
-		*p = i & 0x0f;		// pの番地のメモリに色番号を代入
+	p = (char*)0xa0000; // VRAM開始番地
 
-		// これでwrite_mem8(i, i & 0x0f); の代わりになる
+	for(i=0; i<= 0xffff; i++){
+		*(p + i) = i & 0x0f; // 0xa0000 + i番地にi & 0x0fを代入
 	}
 
 	for(;;){
